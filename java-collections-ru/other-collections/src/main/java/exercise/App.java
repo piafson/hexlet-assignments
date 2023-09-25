7aa40495e77810872e7145728b30e338983b1994
@hexlet/cli <support@hexlet.io>
2023-09-25 14:06:50 +0800

    submit java-collections-ru/other-collections

--- java-collections-ru/other-collections/src/main/java/exercise/App.java
+++ java-collections-ru/other-collections/src/main/java/exercise/App.java
@@ -13,36 +13,29 @@ public class App {
         Map<String, Object> data1, Map<String, Object> data2) {
         TreeMap<String, String> res = new TreeMap<>();
 
-        for (Map.Entry<String, Object> item : data1.entrySet()) {
-            Object d2 = data2.get(item.getKey());
-            if (d2 == null) {
-                continue;
+        data1.forEach((key, value) -> {
+            Object d2 = data2.get(key);
+            if (d2 != null) {
+                if (d2.equals(value)) {
+                    res.put(key, "unchanged");
+                } else {
+                    res.put(key, "changed");
+                }
             }
-            if (d2.equals(item.getValue())) {
-                System.out.println(item.getKey());
-                res.put(item.getKey(), "unchanged");
-            } else {
-                res.put(item.getKey(), "changed");
-            }
-        }
+        });
 
         Set<String> diff1 = new HashSet<>();
         diff1.addAll(data1.keySet());
         diff1.removeAll(data2.keySet());
-
-        for (String s : diff1) {
-            res.put(s, "deleted");
-        }
+        diff1.forEach(key -> { res.put(key, "deleted"); });
 
         Set<String> diff2 = new HashSet<>();
         diff2.addAll(data2.keySet());
         diff2.removeAll(data1.keySet());
-
-        for (String s : diff2) {
-            res.put(s, "added");
-        }
+        diff2.forEach(key -> { res.put(key, "added"); });
 
         return res;
     }
 }
+
 
