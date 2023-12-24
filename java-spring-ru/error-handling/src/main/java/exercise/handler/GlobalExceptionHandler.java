bf11aff0754b1cce5f8638d2c166b3275fd2af4e
@hexlet/cli <support@hexlet.io>
2023-12-24 14:56:03 +0800

    submit java-spring-ru/error-handling

--- java-spring-ru/error-handling/src/main/java/exercise/handler/GlobalExceptionHandler.java
+++ java-spring-ru/error-handling/src/main/java/exercise/handler/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package exercise.handler;
 
+import exercise.exception.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
